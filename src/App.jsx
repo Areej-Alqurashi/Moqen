417783bec6304dffd679085c13759a8e54d744d4
donnadoe0x <donnadoe666@gmail.com>
2025-05-21 11:14:33 +0300

    uppdated

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,13 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import KhutabPage from "./KhutabPage";
 import HomePage from './HomePage';
+import MainPage from './MainPage';
 
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<HomePage />} />
+      <Route path="/" element={<MainPage />} />
       <Route path="/khutab" element={<KhutabPage />} />
     </Routes>
   );
