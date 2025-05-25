132728eeace0c248e9b9a2c719d7bcdd00b4c7bd
Areej Alqurashi <AreejAlqurashi7@gmail.com>
2025-05-25 20:22:53 +0300

    edit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import MainPage from "./MainPage";
